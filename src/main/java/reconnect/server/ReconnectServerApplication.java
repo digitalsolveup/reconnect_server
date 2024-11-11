7d26e4407e89e03eefd1c334543b8ac524c1d71b
Jung <102217402+al1kite@users.noreply.github.com>
2024-11-11 23:11:22 +0900

    fix : exclude MongoAutoConfiguration

--- src/main/java/reconnect/server/ReconnectServerApplication.java
+++ src/main/java/reconnect/server/ReconnectServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @EnableFeignClients
-@SpringBootApplication(exclude = {MongoAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ReconnectServerApplication {
 
     public static void main(String[] args) {
