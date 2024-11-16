8772e9257ef4d7865fb682a222d88681241858e5
ellieeek <ellieeekk@gmail.com>
2024-11-16 15:17:43 +0900

    docs: swagger 주석 설정

--- src/main/java/reconnect/server/domain/missing_person/controller/MissingPersonController.java
+++ src/main/java/reconnect/server/domain/missing_person/controller/MissingPersonController.java
@@ -1,5 +1,7 @@
 package reconnect.server.domain.missing_person.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/missing_person")
+@Tag(name = "MissingPerson", description = "실종자 조회 관련 컨트롤러")
 public class MissingPersonController {
 
     private final MissingPersonService missingPersonService;
@@ -22,6 +25,11 @@ public class MissingPersonController {
     }
 
     // 실종자 목록 조회
+    @Operation(summary = "실종자 목록 조회", description = """
+            실종자 목록을 조회한다.
+            - sortBy: 거리순(DISTANCE), 등록순(REGISTRATION_DATE), 제보 적은순(REPORT_COUNT)
+            - 현재 위도, 경도값 입력
+            """)
     @GetMapping
     public List<MissingPersonListResponse> getMissingPersons(
             @RequestParam(value = "sortBy", defaultValue = "DISTANCE") String sortBy,
@@ -33,16 +41,12 @@ public class MissingPersonController {
 
 
     // 실종자 상세 정보 조회
+    @Operation(summary = "실종자 상세 정보 조회", description = """
+            실종자 상세 정보를 조회한다.
+            - 실종자 고유 id 입력 1,2,3,...
+            """)
     @GetMapping("/{id}")
     public MissingPersonDetailResponse getMissingPersonDetails(@PathVariable("id") Long id) {
         return missingPersonService.getMissingPersonDetails(id);
     }
-
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception e) {
-        e.printStackTrace(); // 로그에 자세한 정보 출력
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error: " + e.getMessage());
-    }
-
 }
